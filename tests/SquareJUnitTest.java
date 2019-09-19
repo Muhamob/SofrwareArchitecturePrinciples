ae9392ae2e603dff6c92d4944d8dc5dc7eaef4f1
muhamob <yusov.aa@phystech.edu>
2019-09-19 19:33:13 +0300

    It looks like, there is no junit 5.5.2 in maven repository, closes #1

--- tests/SquareJUnitTest.java
+++ tests/SquareJUnitTest.java
@@ -1,12 +1,31 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+package tests;
 
+import org.junit.jupiter.api.Test;
 import week2.shapes.Square;
 
-public class SquareJUnitTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareJUnitTest implements ShapeTest {
+
+    private Square square = new Square(1.0, 2.0, 3.0);
+
+    @Override
     @Test
     public void testInit() {
-        Square square = new Square(1.0f, 1.0f, 1.0f);
-        assertEquals(1.0f, square.getX());
+        assertEquals(1.0, square.getX());
+        assertEquals(2.0, square.getY());
+        assertEquals(3.0, square.getLength());
+    }
+
+    @Override
+    @Test
+    public void testSquare() {
+        assertEquals(square.square(), 9.0);
+    }
+
+    @Override
+    @Test
+    public void testPerimeter() {
+        assertEquals(square.perimeter(), 12.0);
     }
 }
